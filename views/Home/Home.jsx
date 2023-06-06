2cd3b200efdf7e907592e8f18bb32ce5d5b2bb59
ManukZatikyan <zatikyan.manuk@mail.ru>
2023-06-06 14:50:06 +0400

    test

--- views/Home/Home.jsx
+++ views/Home/Home.jsx
@@ -2,6 +2,10 @@ import { Text, View } from 'react-native'
 import React from 'react'
 import { styles } from './styles'
 const Home = () => {
+
+
+    var let=4
+    console.log(let)
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Home</Text>
